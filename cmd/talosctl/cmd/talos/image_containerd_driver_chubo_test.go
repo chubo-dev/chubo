5b354806dfa43e2d196aebc3bd83602ed7ae14c8
Francesco Cislaghi <skel84@users.noreply.github.com>
2026-02-12 13:26:10 +0100

    talosctl: test chubo system container driver selection

---
+++ cmd/talosctl/cmd/talos/image_containerd_driver_chubo_test.go
@@ -0,0 +1,21 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+//go:build chubo || chuboos
+
+package talos
+
+import (
+	"testing"
+
+	"github.com/siderolabs/talos/pkg/machinery/api/common"
+)
+
+func TestSystemContainerDriverChubo(t *testing.T) {
+	t.Parallel()
+
+	if got := systemContainerDriver(); got != common.ContainerDriver_CONTAINERD {
+		t.Fatalf("expected CONTAINERD driver, got %v", got)
+	}
+}
