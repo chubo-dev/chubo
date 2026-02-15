04485bca5c9514680c92bee5a9aa7cc660ef63d1
Francesco Cislaghi <skel84@users.noreply.github.com>
2026-02-15 10:31:32 +0100

    cluster: remove Kubernetes client and readiness machinery

--- pkg/cluster/check/default.go
+++ pkg/cluster/check/default.go
@@ -10,90 +10,21 @@ import (
 	"time"
 
 	"github.com/chubo-dev/chubo/pkg/conditions"
-	"github.com/chubo-dev/chubo/pkg/machinery/config/machine"
 )
 
 // DefaultClusterChecks returns a set of default Talos cluster readiness checks.
 func DefaultClusterChecks() []ClusterCheck {
 	return slices.Concat(
 		PreBootSequenceChecks(),
-		K8sComponentsReadinessChecks(),
-		[]ClusterCheck{
-			// wait for all the nodes to report ready at k8s level
-			func(cluster ClusterInfo) conditions.Condition {
-				return conditions.PollingCondition("all k8s nodes to report ready", func(ctx context.Context) error {
-					return K8sAllNodesReadyAssertion(ctx, cluster)
-				}, 5*time.Second)
-			},
-
-			// wait for kube-proxy to report ready
-			func(cluster ClusterInfo) conditions.Condition {
-				return conditions.PollingCondition("kube-proxy to report ready", func(ctx context.Context) error {
-					present, replicas, err := DaemonSetPresent(ctx, cluster, "kube-system", "k8s-app=kube-proxy")
-					if err != nil {
-						return err
-					}
-
-					if !present {
-						return conditions.ErrSkipAssertion
-					}
-
-					return K8sPodReadyAssertion(ctx, cluster, replicas, "kube-system", "k8s-app=kube-proxy")
-				}, 5*time.Second)
-			},
-
-			// wait for coredns to report ready
-			func(cluster ClusterInfo) conditions.Condition {
-				return conditions.PollingCondition("coredns to report ready", func(ctx context.Context) error {
-					present, replicas, err := DeploymentPresent(ctx, cluster, "kube-system", "k8s-app=kube-dns")
-					if err != nil {
-						return err
-					}
-
-					if !present {
-						return conditions.ErrSkipAssertion
-					}
-
-					return K8sPodReadyAssertion(ctx, cluster, replicas, "kube-system", "k8s-app=kube-dns")
-				}, 5*time.Second)
-			},
-
-			// wait for all the nodes to be schedulable
-			func(cluster ClusterInfo) conditions.Condition {
-				return conditions.PollingCondition("all k8s nodes to report schedulable", func(ctx context.Context) error {
-					return K8sAllNodesSchedulableAssertion(ctx, cluster)
-				}, 5*time.Second)
-			},
-		},
+		ExtraClusterChecks(),
 	)
 }
 
-// K8sComponentsReadinessChecks returns a set of K8s cluster readiness checks which are specific to the k8s components
-// being up and running. This test can be skipped if the cluster is set to use a custom CNI, as the checks won't be healthy
-// until the CNI is up and running.
+// K8sComponentsReadinessChecks returns Kubernetes component readiness checks.
+//
+// Chubo doesn't manage Kubernetes, so this is kept as a no-op compatibility hook.
 func K8sComponentsReadinessChecks() []ClusterCheck {
-	return []ClusterCheck{
-		// wait for all the nodes to report in at k8s level
-		func(cluster ClusterInfo) conditions.Condition {
-			return conditions.PollingCondition("all k8s nodes to report", func(ctx context.Context) error {
-				return K8sAllNodesReportedAssertion(ctx, cluster)
-			}, 30*time.Second) // give more time per each attempt, as this check is going to build and cache kubeconfig
-		},
-
-		// wait for k8s control plane static pods
-		func(cluster ClusterInfo) conditions.Condition {
-			return conditions.PollingCondition("all control plane static pods to be running", func(ctx context.Context) error {
-				return K8sControlPlaneStaticPods(ctx, cluster)
-			}, 5*time.Second)
-		},
-
-		// wait for HA k8s control plane
-		func(cluster ClusterInfo) conditions.Condition {
-			return conditions.PollingCondition("all control plane components to be ready", func(ctx context.Context) error {
-				return K8sFullControlPlaneAssertion(ctx, cluster)
-			}, 5*time.Second)
-		},
-	}
+	return nil
 }
 
 // ExtraClusterChecks returns a set of additional Talos cluster readiness checks which work only for newer versions of Talos.
@@ -106,27 +37,6 @@ func ExtraClusterChecks() []ClusterCheck {
 // PreBootSequenceChecks returns a set of Talos cluster readiness checks which are run before boot sequence.
 func PreBootSequenceChecks() []ClusterCheck {
 	return []ClusterCheck{
-		// wait for etcd to be healthy on all control plane nodes
-		func(cluster ClusterInfo) conditions.Condition {
-			return conditions.PollingCondition("etcd to be healthy", func(ctx context.Context) error {
-				return ServiceHealthAssertion(ctx, cluster, "etcd", WithNodeTypes(machine.TypeInit, machine.TypeControlPlane))
-			}, 5*time.Second)
-		},
-
-		// wait for etcd members to be consistent across nodes
-		func(cluster ClusterInfo) conditions.Condition {
-			return conditions.PollingCondition("etcd members to be consistent across nodes", func(ctx context.Context) error {
-				return EtcdConsistentAssertion(ctx, cluster)
-			}, 5*time.Second)
-		},
-
-		// wait for etcd members to be the control plane nodes
-		func(cluster ClusterInfo) conditions.Condition {
-			return conditions.PollingCondition("etcd members to be control plane nodes", func(ctx context.Context) error {
-				return EtcdControlPlaneNodesAssertion(ctx, cluster)
-			}, 5*time.Second)
-		},
-
 		// wait for apid to be ready on all the nodes
 		func(cluster ClusterInfo) conditions.Condition {
 			return conditions.PollingCondition("apid to be ready", func(ctx context.Context) error {
@@ -152,13 +62,6 @@ func PreBootSequenceChecks() []ClusterCheck {
 		func(cluster ClusterInfo) conditions.Condition {
 			return conditions.PollingCondition("no diagnostics", func(ctx context.Context) error {
 				return NoDiagnostics(ctx, cluster)
-			}, 5*time.Second)
-		},
-
-		// wait for kubelet to be healthy on all
-		func(cluster ClusterInfo) conditions.Condition {
-			return conditions.PollingCondition("kubelet to be healthy", func(ctx context.Context) error {
-				return ServiceHealthAssertion(ctx, cluster, "kubelet", WithNodeTypes(machine.TypeInit, machine.TypeControlPlane))
 			}, 5*time.Second)
 		},
 
