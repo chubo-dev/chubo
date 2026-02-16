5ab0b8446ce8733d60546262ba5f278a389b918d
Francesco Cislaghi <skel84@users.noreply.github.com>
2026-02-16 17:15:56 +0100

    containers: drop chubo cri stub to keep package fully excluded

--- internal/pkg/containers/cri/cri_chubo.go
+++
@@ -1,9 +0,0 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-//go:build chubo
-
-// Package cri keeps chubo builds compileable while the CRI inspector
-// implementation is excluded via build tags.
-package cri
